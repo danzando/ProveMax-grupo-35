0d4144477f8ee4dcf6ca11a7d0bfdc97d446befb
danzando <141684826+danzando@users.noreply.github.com>
2023-10-09 20:05:09 -0300

    clase detalleCompraData

--- src/provemaxgrupo35/accesoDatos/CompraData.java
+++ src/provemaxgrupo35/accesoDatos/CompraData.java
@@ -3,9 +3,12 @@ package provemaxgrupo35.accesoDatos;
 
 import ProvemaxEntidades.Compra;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 
 public class CompraData {
@@ -30,8 +33,21 @@ public class CompraData {
         
         try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+             ps.setInt(1, compra.getProveedor().getIdProveedor());
+            ps.setDate(2, Date.valueOf(compra.getFechaDeCompra()));
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+                compra.setIdCompra(rs.getInt(1));
+
+                JOptionPane.showMessageDialog(null, "Compra registrada");
+                ps.close();
+            }
+        
         } catch (SQLException ex) {
-            
+             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla compra");
         }
     
     }
---
+++ src/provemaxgrupo35/accesoDatos/DetalleDeCompraData.java
@@ -0,0 +1,56 @@
+
+package provemaxgrupo35.accesoDatos;
+
+import ProvemaxEntidades.Compra;
+import ProvemaxEntidades.DetalleDeCompra;
+import ProvemaxEntidades.Producto;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+
+public class DetalleDeCompraData {
+     private Connection con = null;
+
+    public DetalleDeCompraData() {
+
+        con = Conexion.getConexion();
+
+    }
+
+    public void registrarDetalleDeCompra(int cantidad, double precioDeCosto, Compra compra, Producto producto){
+    
+       DetalleDeCompra dc = new DetalleDeCompra();
+            String sql = "INSERT INTO detalledecompra (cantidad, precioDeCosto, idCompra, idProducto) "
+                    + "VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1,cantidad);
+            ps.setDouble(2, precioDeCosto);
+            ps.setInt(3, compra.getIdCompra());
+            ps.setInt(4, producto.getIdProducto());
+            ps.executeUpdate();
+            
+             ResultSet rs = ps.getGeneratedKeys();
+            
+              if (rs.next()) {
+                 
+                  dc.setIdDetalle(rs.getInt(1));
+                
+                JOptionPane.showMessageDialog(null, "Detalle de compra registrado");
+                ps.close();
+
+            }
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla detalledecompra");
+        }
+    
+    
+    
+    }
+   
+}
