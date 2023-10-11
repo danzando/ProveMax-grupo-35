5cf2f28c9b380226c893959fc05c46066243d0ce
Meliey04 <melisaleila04@gmail.com>
2023-10-10 21:26:26 -0300

    pruebas en el main
    métodos buscar compra, listar, compras y listar compras por proveedor

--- src/provemax/grupo/pkg35/ProvemaxGrupo35.java
+++ src/provemax/grupo/pkg35/ProvemaxGrupo35.java
@@ -1,47 +1,68 @@
-
 package provemax.grupo.pkg35;
 
+import ProvemaxEntidades.Compra;
 import ProvemaxEntidades.Producto;
+import ProvemaxEntidades.Proveedor;
+import java.time.LocalDate;
+import provemaxgrupo35.accesoDatos.CompraData;
 import provemaxgrupo35.accesoDatos.ProductoData;
-
+import provemaxgrupo35.accesoDatos.ProveedorData;
 
 public class ProvemaxGrupo35 {
 
-  
     public static void main(String[] args) {
-       
-        ProductoData proDa = new ProductoData();
-        
+
+//        ProductoData proDa = new ProductoData();
         // REGISTRAR PRODUCTO:
         //Producto p = new Producto("Batidora", "Liliana", 30000, 15, true);
-         //proDa.registrarProducto(p);
-         
-         
-          //BUSCAR PRODUCTO
-         //proDa.buscarProducto(11);
-             // System.out.println(proDa.buscarProducto(11));
-              
-              
-         //MODIFICAR PRODUCTO
-        Producto p = new Producto(11,"Ventilador", "Liliana", 30000, 15, true);
-         proDa.modificarProducto(p); 
-         System.out.println(p);
-
-         
-         
+        //proDa.registrarProducto(p);
+        //BUSCAR PRODUCTO
+        //proDa.buscarProducto(11);
+        // System.out.println(proDa.buscarProducto(11));
+        //MODIFICAR PRODUCTO
+//        Producto p = new Producto(11,"Ventilador", "Liliana", 30000, 15, true);
+//         proDa.modificarProducto(p); 
+//         System.out.println(p);
         //LISTAR PRODUCTOS
         //  proDa.listarProductos();   
         //  for (Producto producto: proDa.listarProductos()) {
         //      System.out.println(producto);
         // }
-        
-        
         // ELIMINAR PRODUCTO
-        
         //proDa.eliminarProducto(11);
-        
-         
-         
+        //REGISTRAR PROVEEDOR
+        ProveedorData pd = new ProveedorData();
+//        Proveedor pro= new Proveedor("Maxiconsumo","Ruta 7 K30","2664444444","maxiconsumo@gmail.com");
+//       pd.registrarProveedor(pro);
+
+//BUSCAR PROVEEDOR
+//System.out.println(pd.buscarProveedor(1));
+//MODIFICAR PROVEEDOR
+//         Proveedor prob= new Proveedor(8,"Maxiconsumo","Ruta 7","2664444444","maxiconsumo@gmail.com");
+//         pd.modificarProveedor(prob);
+//         System.out.println(prob);
+//LISTAR PROVEEDOR
+//for (Proveedor proveedor: pd.listarProveedores()) {
+//             System.out.println(proveedor);
+        //REGISTRAR COMPRA
+//    Proveedor pro=pd.buscarProveedor(1);
+        CompraData cd = new CompraData();
+//          LocalDate fecha = LocalDate.of(2023, 04, 01);
+//          Compra com= new Compra(pro,fecha);
+//          cd.registrarCompra(com);
+
+//BUSCAR COMPRA
+//        System.out.println(cd.buscarCompra(5));
+//
+//        //LISTAR COMPRAS
+//     
+//        for (Compra compras : cd.ListarCompras()) {
+//            System.out.println(compras);
+//        }
+//COMPRAS POR PROVEEDOR
+        Proveedor pro = pd.buscarProveedor(10);
+         for (Compra xproveedor: cd.ComprasPorProveedor(pro)) {
+            System.out.println(xproveedor);
     }
-    
+    }
 }
--- src/provemaxgrupo35/accesoDatos/CompraData.java
+++ src/provemaxgrupo35/accesoDatos/CompraData.java
@@ -1,18 +1,19 @@
-
 package provemaxgrupo35.accesoDatos;
 
 import ProvemaxEntidades.Compra;
+import ProvemaxEntidades.Proveedor;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
-
 public class CompraData {
-    
 
     private Connection con = null;
 
@@ -21,19 +22,18 @@ public class CompraData {
         con = Conexion.getConexion();
 
     }
-    
+
     // realizar compra: producto solicitado, la cantidad y la fecha del pedido 
     // listar productos comprados en determinada fecha  / ultima compra
     // listar compras a proveedor x : public List<Inscripcion> obtenerCompraPorProveedor(int idProveedor)
     //
-    
-    public void registrarCompra (Compra compra){
-        String sql = "INSERT INTO compra (idProveedor, fechaDeCompra)"
+    public void registrarCompra(Compra compra) {
+        String sql = "INSERT INTO compra (idProveedor, fechaDeCompra) "
                 + "VALUES (?,?)";
-        
+
         try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-             ps.setInt(1, compra.getProveedor().getIdProveedor());
+            ps.setInt(1, compra.getProveedor().getIdProveedor());
             ps.setDate(2, Date.valueOf(compra.getFechaDeCompra()));
             ps.executeUpdate();
 
@@ -45,10 +45,116 @@ public class CompraData {
                 JOptionPane.showMessageDialog(null, "Compra registrada");
                 ps.close();
             }
-        
+
         } catch (SQLException ex) {
-             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla compra");
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla compra" + ex);
         }
-    
+
     }
+//     Buscar Compra
+
+    public Compra buscarCompra(int idCompra) {
+
+        String sql = "SELECT idProveedor, fechaDeCompra  FROM Compra WHERE idCompra=? ";
+        ProveedorData pb = new ProveedorData();
+        Compra com = null;
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idCompra);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+
+                com = new Compra();  //esta de mas?
+                com.setIdCompra(idCompra);
+                com.setProveedor(pb.buscarProveedor(rs.getInt("idProveedor")));
+                 java.sql.Date fechaSql = rs.getDate("fechaDeCompra");
+                LocalDate fecha = fechaSql.toLocalDate();
+                com.setFechaDeCompra(fecha);
+            } else {
+
+                JOptionPane.showMessageDialog(null, "No existe la compra");
+
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a tabla compra");
+        }
+
+        return com;
+        //
+           }
+
+    public List<Compra> ListarCompras() {
+        ArrayList<Compra> compras = new ArrayList<>();
+
+        String sql = "select * from compra ";
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) { // mientras haya filas en rs  ejecuto el while  
+                Compra com = new Compra();
+                ProveedorData pd = new ProveedorData();
+
+                com.setIdCompra(rs.getInt("idCompra"));
+
+                Proveedor pro = pd.buscarProveedor(rs.getInt("idProveedor"));
+                java.sql.Date fechaSql = rs.getDate("fechaDeCompra");
+                LocalDate fecha = fechaSql.toLocalDate();
+                com.setProveedor(pro);
+                com.setFechaDeCompra(fecha);
+
+                compras.add(com);
+
+            }
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion a tabla compras");
+        }
+
+        return compras;
+    }
+
+//        compra por proveedor
+    public List<Compra> ComprasPorProveedor(Proveedor proveedor) {
+        ArrayList<Compra> compras = new ArrayList<>();
+
+        String sql = "select * from compra where IdProveedor=? ";
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, proveedor.getIdProveedor());
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) { // mientras haya filas en rs  ejecuto el while  
+                Compra com = new Compra();
+                ProveedorData pd = new ProveedorData();
+
+                com.setIdCompra(rs.getInt("idCompra"));
+
+                Proveedor pro = pd.buscarProveedor(proveedor.getIdProveedor());
+                java.sql.Date fechaSql = rs.getDate("fechaDeCompra");
+                LocalDate fecha = fechaSql.toLocalDate();
+                com.setProveedor(pro);
+                com.setFechaDeCompra(fecha);
+
+                compras.add(com);
+
+            }
+
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion a tabla compras");
+        }
+
+        return compras;
+    }
+
 }
+
